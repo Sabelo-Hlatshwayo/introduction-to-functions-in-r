009fad8b99277a05649347ef3a6134eb21786163
Sabelo Hlatshwayo <evidence.hlatshwayo@gmail.com>
2023-05-08 11:21:22 +0200

    Change the name of the function in Exercise 1 from caclulateCircumference to myFunction1

---
+++ Introduction-To-Functions-In-R.R
@@ -0,0 +1,15 @@
+# Exercise 1
+# Create a function to calculate the circumference of a circle with a known radius using the formula 2πr. In this 
+# example the function has only one parameter, namely r. After defining the function, we will call it with a 
+# radius equal to 2, i.e., with the argument {2}.
+
+myFunction1 <- function(radius) {
+  return(2*pi*radius)
+}
+
+myFunction1(2);
+
+
+
+
+
